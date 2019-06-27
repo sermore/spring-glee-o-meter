42494e36606fa6becd127ab4f5a6eac41317506e
Sergio Moretti <sergio@reliqs.net>
2019-06-28 01:50:52 +0200

    Http security fix, moved http security to resource server configuration in order to make it working correctly.

--- src/main/java/net/reliqs/gleeometer/security/ResourceServerConfiguration.java
+++ src/main/java/net/reliqs/gleeometer/security/ResourceServerConfiguration.java
@@ -1,6 +1,10 @@
 package net.reliqs.gleeometer.security;
 
+import net.reliqs.gleeometer.errors.CustomAccessDeniedHandler;
+import net.reliqs.gleeometer.errors.CustomAuthenticationEntryPoint;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
@@ -8,10 +12,32 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+
+    public ResourceServerConfiguration(CustomAuthenticationEntryPoint customAuthenticationEntryPoint) {
+        this.customAuthenticationEntryPoint = customAuthenticationEntryPoint;
+    }
 
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) {
         resources.resourceId("api");
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .antMatcher("/api/**")
+                .authorizeRequests()
+                .antMatchers("/api/signin**").permitAll()
+                .antMatchers("/api/signin/**").permitAll()
+                .antMatchers("/api/glee**").hasAnyAuthority("ADMIN", "USER")
+                .antMatchers("/api/users**").hasAuthority("ADMIN")
+                .antMatchers("/api/**").authenticated()
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling().authenticationEntryPoint(customAuthenticationEntryPoint).accessDeniedHandler(new CustomAccessDeniedHandler());
     }
 
 }
--- src/main/java/net/reliqs/gleeometer/security/ServerSecurityConfig.java
+++ src/main/java/net/reliqs/gleeometer/security/ServerSecurityConfig.java
@@ -1,35 +1,29 @@
 package net.reliqs.gleeometer.security;
 
-import net.reliqs.gleeometer.errors.CustomAccessDeniedHandler;
 import net.reliqs.gleeometer.errors.CustomAuthenticationEntryPoint;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-@Order(1)
+//@Order(1)
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, proxyTargetClass = true)
 public class ServerSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
 
     private final UserDetailsService userDetailsService;
 
     public ServerSecurityConfig(CustomAuthenticationEntryPoint customAuthenticationEntryPoint, @Qualifier("userService")
             UserDetailsService userDetailsService) {
-        this.customAuthenticationEntryPoint = customAuthenticationEntryPoint;
         this.userDetailsService = userDetailsService;
     }
 
@@ -50,23 +44,6 @@ public class ServerSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .antMatcher("/api/**")
-                .authorizeRequests()
-                .antMatchers("/api/signin/**").permitAll()
-                .antMatchers("/api/glee/**").hasAnyAuthority("ADMIN", "USER")
-                .antMatchers("/api/users/**").hasAuthority("ADMIN")
-                .antMatchers("/api/**").authenticated()
-                .anyRequest().authenticated()
-                .and()
-                .csrf().disable()
-                .exceptionHandling().authenticationEntryPoint(customAuthenticationEntryPoint).accessDeniedHandler(new CustomAccessDeniedHandler());
     }
 
 }
